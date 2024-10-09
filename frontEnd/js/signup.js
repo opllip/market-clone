2460a60da238e3b18ff77e0cd7d570edb2085897
shailun <57696595+shailun@users.noreply.github.com>
2024-10-09 11:59:26 +0900

    회원가입 프론트단 1차 커밋
    -회원가입 아직 아이디 중복체크 없음
    - 회원가입 폼 전송 시 db저장까지 완료
    - js 파일 별도 폴더로 이동

--- frontEnd/js/signup.js
+++ frontEnd/js/signup.js
@@ -16,6 +16,9 @@ const handleSubmit = async (event) => {
   formData.set("password", sha256Pw);
   const div = document.getElementById("info");
 
+  //아이디 중복을 체크할것!!
+
+  //비밀번호 체크
   if (checkPw()) {
     div.innerText = "";
     const res = await fetch("/signup", {
@@ -26,6 +29,8 @@ const handleSubmit = async (event) => {
     if (data === "200") {
       div.innerText = "가입이 완료되었습니다.";
       div.style.color = "blue";
+      alert("회원가입에 성공했습니다."); //옛날방식이라 요즘은 쓰지 않음. 요즘은 토스트를 씀
+      window.location.pathname = "/login.html";
     }
   } else {
     div.innerText = "비밀번호가 일치하지 않습니다.";
--- main.py
+++ main.py
@@ -68,10 +68,19 @@ async def get_items():
 
   
     
-# 회원가입
+# 회원가입 --------------------------------------
 @app.post("/signup")
-def signup(id:Annotated[str, Form()], password:Annotated[str, Form()]):
-    print(id, password)
+def signup(id:Annotated[str, Form()], 
+           password:Annotated[str, Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+
+    cur.execute(f"""
+               INSERT INTO users
+               (id, name, email, password)
+               VALUES ('{id}' ,'{name}','{email}','{password}') 
+                """)
+    con.commit()
     return '200'
 
 
